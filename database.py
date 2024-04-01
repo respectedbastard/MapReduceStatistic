3b030e2e51b0fbaa37b4b64b41417d2a18c4e713
respectedbastard <cicazus@mail.ru>
2024-04-01 22:34:24 +0300

    Refactor database settings

--- database.py
+++ database.py
@@ -1,9 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-DATABASE_URL = "postgresql+psycopg2://postgres:1917@localhost:5432/mapreduce"
+class Settings(BaseSettings):
+    user: str
+    password: str
+    host: str
+    port: str
+    db_name: str
 
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+def get_settings():
+    return Settings()
+
+settings = get_settings()
+
+DATABASE_URL = f"postgresql+psycopg2://{settings.user}:{settings.password}@{settings.host}:{settings.port}/{settings.db_name}"
 
 engine = create_engine(DATABASE_URL)
 
